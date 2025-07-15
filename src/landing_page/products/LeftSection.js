41eef6c0ca22bf5b629ab66a49184ea0fe99d64a
Jash-1th <jaswanthmamilla@gmail.com>
2025-07-15 08:25:34 +0530

    products component structures

--- src/landing_page/products/LeftSection.js
+++ src/landing_page/products/LeftSection.js
@@ -1,6 +1,46 @@
-import React from 'react';
-function LeftSection() {
-    return ( <h1>LeftSection</h1>  );
+import React from "react";
+
+function LeftSection({
+  imageURL,
+  productName,
+  productDesription,
+  tryDemo,
+  learnMore,
+  googlePlay,
+  appStore,
+}) {
+  return (
+   
+      <div className="row p-md-5">
+        <div className="col-md-6 col-12 ">
+          <img  src={imageURL} />
+        </div>
+        <div className="col-md-6 col-12  p-md-5 mt-5">
+          <h1>{productName}</h1>
+          <p>{productDesription}</p>
+          <div>
+            <a href={tryDemo}>Try Demo</a>
+            <a href={learnMore} style={{ marginLeft: "50px" }}>
+              Learn More
+            </a>
+          </div>
+          <div className="mt-3">
+            {googlePlay &&<a href={googlePlay}>
+              <img  src="media/googlePlayBadge.svg" />
+            </a>
+}
+            {appStore&&<a href={appStore}>
+              <img 
+                src="media/appstoreBadge.svg"
+                style={{ marginLeft: "50px" }}
+              />
+            </a>
+}
+          </div>
+        </div>
+      </div>
+  
+  );
 }
 
 export default LeftSection;
--- src/landing_page/products/Universe.js
+++ src/landing_page/products/Universe.js
@@ -1,6 +1,49 @@
-import React from 'react';
+import React from "react";
+
 function Universe() {
-    return ( <h1>Universe</h1>  );
+  return (
+    
+      <div className="row  text-center mt-5">
+        <h1>The Zerodha Universe</h1>
+        <p>
+          Extend your trading and investment experience even further with our
+          partner platforms
+        </p>
+        <div className="row row-cols-md-3 row-cols-sm-2">
+        <div className=" col-md-4 img-content  p-md-3 mt-5">
+          <img src="media/smallcaseLogo.png"  />
+          <p className="text-small text-muted">Thematic investment platform</p>
+        </div>
+        <div className="col-md-4 img-content  p-md-3 mt-5">
+          <img src="media/zerodhaFundhouse.png" />
+          <p className="text-small text-muted">our assest management venture that is creating simple and transparent index funds to help you save for your goals</p>
+        </div>
+        <div className="col-md-4 img-content  p-md-3 mt-5">
+          <img src="media/sensibullLogo.svg" />
+          <p className="text-small text-muted">options trading platform that lets you create strategies , analyze positions , and examine data points like open intreset , FII/DII , and more</p>
+        </div>
+        <div className="col-md-4 img-content  p-md-3 mt-5">
+          <img src="media/tijori.svg" />
+          <p className="text-small text-muted">Invest research platform that offers detailed insight on stocks , sectors, supply chains, and more</p>
+        </div>
+        <div className="col-md-4 img-content  p-md-3 mt-5">
+          <img src="media/streakLogo.png" />
+          <p className="text-small text-muted">Systematic trading platform that allows you to create and backtest strategies without coding</p>
+        </div>
+        <div className="col-md-4 img-content  p-md-3 mt-5">
+          <img src="media/dittoLogo.png" />
+          <p className="text-small text-muted">Personalized advice on life and health insurance. No spam and no mis-selling.</p>
+        </div>
+        </div>
+        <button
+          className="p-2  btn btn-primary mb-5 mt-5"
+          style={{ width: "20%", margin: "0 auto" }}
+        >
+          Signup Now
+        </button>
+      </div>
+  
+  );
 }
 
 export default Universe;
