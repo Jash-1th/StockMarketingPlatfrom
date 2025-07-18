0ef2605c81ef017ee85c30d4058e3edd1c21f528
Jash-1th <jaswanthmamilla@gmail.com>
2025-07-18 08:19:05 +0530

    connected with mongodb

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+require("dotenv").config();
+console.log(process.env.MONGODB_URL);
 const express = require("express");
+const mongoose =  require("mongoose");
+
+
 
 
 const app = express();
+const mongoDb_URL= process.env.MONGODB_URL;
+const port = process.env.PORT || 3002;
 
-app.listen(3000 , ()=>{
-    console.log("lisiening on the port" , 300)
+mongoose.connect(mongoDb_URL)
+.then(()=>{console.log("connection with mongodb successfully made")})
+.catch((err )=>{console.log(err);});
+
+app.listen(port , ()=>{
+    console.log("lisiening on the port" , port)
 })
